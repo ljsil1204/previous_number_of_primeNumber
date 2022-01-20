2e4684df4e39a605461c505993aa2694f2cd35a9
이진실 <ljsil1204@naver.com>
2022-01-20 23:57:50 +0900

    숫자 입력하기
    - 스캐너 변수생성

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,5 +1,18 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
+	public static void main(String[] args) {
+		
+//		[응용문제 3] 소수 판별기 - 입력받은 숫자 직전의 소수가 어떤건지? (제일 큰 소수?)
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("숫자 입력 : ");
+		int num = myScanner.nextInt();
+		
+	}
+	
 }
